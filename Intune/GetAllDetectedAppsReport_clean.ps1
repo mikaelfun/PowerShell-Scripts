9ee70747254c2111808119b4e36f67be9fde5efc
Kun Fang <31831389+mikaelfun@users.noreply.github.com>
2022-07-20 16:14:15 +0800

    rcc

---
+++ Intune/GetAllDetectedAppsReport_clean.ps1
@@ -0,0 +1,135 @@
+﻿<#
+
+
+
+Author:
+
+#>
+
+$apiVersion = "beta"
+
+function ConnectToGraph
+{
+    if (Get-Module -ListAvailable -Name Microsoft.Graph.Intune) 
+    {
+    } 
+    else {
+        Write-Host "Microsoft.Graph.Intune Module does not exist, installing..."
+        Install-Module -Name Microsoft.Graph.Intune
+    }
+    
+    
+    Connect-MSGraph
+}
+
+
+function Get_GraphURL($URL)
+{
+    for ($CallCount =0; $CallCount -lt 30; $CallCount++) 
+    {
+        Try # graph call will sometimes return 503 service unavailable due to dense requests.
+        {
+            $Response = invoke-MSGraphRequest -HttpMethod GET -Url $URL
+			return $Response
+        }
+        Catch 
+        {
+			Write-Host($_)
+            Continue
+        }
+    }
+    return $null
+}
+
+
+function Post_GraphURL($URL, $Body)
+{
+    Try
+    {
+        invoke-MSGraphRequest -HttpMethod POST -Url $URL -Content $Body
+        return $TRUE
+    }
+    Catch 
+    {
+        Write-Host($_)
+        return $FALSE
+    }
+    return $FALSE
+}
+
+
+
+function main
+{
+    $URL = "https://graph.microsoft.com/beta/deviceManagement/detectedApps"
+
+    $allDetectedApps = Get_GraphURL($URL)
+    $AppList = $allDetectedApps.value
+    $appNum = $allDetectedApps.value.Length
+    
+    $curIndex = 0
+    $num = $allDetectedApps.'@odata.count'
+    while ($allDetectedApps.'@odata.nextLink')
+    {
+        $allDetectedApps = Get_GraphURL($allDetectedApps.'@odata.nextLink')
+        $AppList += $allDetectedApps.value
+        $appNum += $allDetectedApps.value.Length
+        
+        $curIndex++
+        $i1 = [int]($curIndex * 50 / $num * 100)
+        Write-Progress -Activity "Detected Apps in Progress" -Status "$i1% Complete" -PercentComplete $i1;
+    }
+
+    $InterestedAppPool = 'Android Setup','mlp'
+    
+    $AppList = $AppList | Where-Object -FilterScript {($_.displayName -in $InterestedAppPool)} | select id,displayName, version
+
+    $output = @()
+    $num = $AppList.Length
+    $curIndex = 0
+    foreach ($eachDetectedApp in $AppList)
+    {
+        $URL = “https://graph.microsoft.com/beta/deviceManagement/detectedApps/"+$eachDetectedApp.id+"/managedDevices"
+        $CurrentDevices = Get_GraphURL($URL)
+
+        $DeviceList = $CurrentDevices.value
+        $DeviceNum = $CurrentDevices.value.Length
+    
+        $curIndex = 0
+        $num = $CurrentDevices.'@odata.count'
+        while ($CurrentDevices.'@odata.nextLink')
+        {
+            $CurrentDevices = Get_GraphURL($CurrentDevices.'@odata.nextLink')
+            $DeviceList += $CurrentDevices.value
+            $DeviceNum += $CurrentDevices.value.Length
+        
+            $curIndex++
+            $i2 = [int]($curIndex * 50 / $num * 100)
+            Write-Progress -Activity "$eachDetectedApp.displayName: Detected App Device search in Progress" -Status "$i2% Complete" -PercentComplete $i2;
+        }
+
+
+        foreach ($eachdevice in $DeviceList)
+        {
+            $Result = "" | Select AppName, AppVersion, DeviceName, User, lastSync
+            $Result.AppName = $eachDetectedApp.displayName
+            $Result.AppVersion = $eachDetectedApp.version
+            $Result.DeviceName = $eachdevice.deviceName
+            $Result.User = $eachdevice.emailAddress
+            $Result.lastSync = $eachdevice.lastSyncDateTime
+            $output += $Result
+        }
+        $curIndex++
+        $i3 = [int]($curIndex / $num * 100)
+        Write-Progress -Activity "Detected Apps Report in Progress" -Status "$i3% Complete" -PercentComplete $i3;
+    }
+
+    $output | Export-Csv -Path "C:\AllDetectedApps.csv"
+    $output | Format-Table
+    
+
+    Read-Host 'Press Enter to exit…'
+}
+
+
+main
